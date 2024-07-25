1c77275a1604d5631f9ce0de90770c2d1b64dda2
Shivam Sharma <shivamsharma68385@gmail.com>
2024-07-26 01:29:32 +0530

    loader issue checking

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,7 +1,7 @@
 const Loader = () => {
   return (
     <div className="fixed top-0 bg-black left-0 h-full w-[100vw] object-cover z-50">
-        <video className="h-screen w-[100vw]" autoPlay loop muted src="https://github.com/ShivamS28/FellowshipFrontiers-ReimagineRound2/blob/main/src/assets/something.mp4"></video>
+        <video className="h-screen w-[100vw]" autoPlay loop muted src=".main/src/assets/something.mp4"></video>
     </div>
   )
 }
